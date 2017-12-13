f09d406a8e7b6e7bbf90865cfc58c2e62e0ff292
AntonBock <34304728+AntonBock@users.noreply.github.com>
2017-12-13 10:38:51 +0100

    Update subscriber.cpp

--- subscriber/src/subscriber.cpp
+++ subscriber/src/subscriber.cpp
@@ -2,25 +2,50 @@
 #include "ros/console.h"
 #include "std_msgs/String.h"
 #include "iostream"
+#include "string"
+#include "vector"
 
 class Exam
 {
   ros::NodeHandle nh;
   ros::Subscriber sub;
   std::string word;
+  std::vector<std::string> wordList;
+  int chosenPos;
+  std::string chosenWord;
 
 private:
+void question(){
+  chosenPos = 0;
+  chosenWord = wordList[chosenPos];
+  ROS_INFO_STREAM(chosenWord);
+}
+
+void compare (){
+    if(word.compare(chosenWord) != 0){
+      ROS_INFO_STREAM("Wrong");
+    }
+    else{
+      ROS_INFO_STREAM("Correct");
+    }
+    question();
+}
+
 void sbCallback (const std_msgs::String::ConstPtr& msg){
   //insert data from message into word string
   word=msg->data;
   //output word onto the terminal
   ROS_INFO_STREAM(word);
+  compare();
 }
 
 public:
   //Exam constructor
   Exam()
   {
+    wordList.push_back("toast");
+    question();
+    //wordList[] = {"toast", "beers", "trees"};
   sub = nh.subscribe("publisher", 10, &Exam::sbCallback,this);
   };
 //Exam deconstructor
